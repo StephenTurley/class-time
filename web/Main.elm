011c05b6d832c9efdf12f9143c52240f4c6fbd74
Stephen Turley <stephen.turley@protonmail.com>
2020-09-09 13:52:03 -0400

    added a hard coded event and printed its info out

--- web/Main.elm
+++ web/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (..)
 
 import Browser exposing (Document)
 import Html exposing (..)
+import Html.Attributes exposing (href)
+import Task exposing (Task)
 import Time
 
 
@@ -36,7 +38,17 @@ type alias Event =
 
 init : () -> ( Model, Cmd msg )
 init _ =
-    ( [], Cmd.none )
+    let
+        start =
+            Time.millisToPosix 1599673056201
+
+        end =
+            Time.millisToPosix (1599673056201 + (60 * 60 * 1000))
+
+        event =
+            Event start end "http://zoom.com" "Test"
+    in
+    ( [ event ], Cmd.none )
 
 
 
@@ -48,8 +60,36 @@ update msg model =
     ( model, Cmd.none )
 
 
+
+-- view
+
+
 view : Model -> Document msg
 view model =
     { title = "Class Time"
-    , body = [ h2 [] [ text "hello world" ] ]
+    , body =
+        [ h2 [] [ text "Today's schedule" ]
+        , div [] [ ul [] (List.map eventItem model) ]
+        ]
     }
+
+
+eventItem : Event -> Html msg
+eventItem event =
+    li []
+        [ a [ href event.url ] [ text event.title ]
+        , p [] [ strong [] [ text "Start Time " ], text (humanTime event.startTime) ]
+        , p [] [ strong [] [ text "End Time " ], text (humanTime event.endTime) ]
+        ]
+
+
+humanTime : Time.Posix -> String
+humanTime time =
+    let
+        hour =
+            String.fromInt (Time.toHour Time.utc time)
+
+        minute =
+            String.fromInt (Time.toMinute Time.utc time)
+    in
+    hour ++ ":" ++ minute
