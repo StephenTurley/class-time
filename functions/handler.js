4b6197b77c987b276fe39f6db9108509244d5206
Stephen Turley <stephen.turley@protonmail.com>
2020-09-11 15:48:11 -0400

    added a hardcoded schedule response to the lambda function

--- functions/handler.js
+++ functions/handler.js
@@ -1,10 +1,26 @@
 'use strict'
 
+const createEvent = (title, url, startHour, startMinute, durationInMinutes) => {
+  return {
+    title: title,
+    url: url,
+    startHour: startHour,
+    startMinute: startMinute,
+    durationInMinutes: durationInMinutes
+  }
+}
+
 module.exports.schedule = (event, context, callback) => {
+  const schedule = [
+    createEvent('first', 'http://google.com', 8, 30, 30),
+    createEvent('third', 'http://google.com', 9, 30, 30),
+    createEvent('last', 'http://google.com', 15, 30, 30),
+    createEvent('second', 'http://google.com', 9, 0, 30)
+  ]
   callback(null, {
     statusCode: 200,
     isBase64Encoded: false,
     headers: {},
-    body: 'Hello'
+    body: JSON.stringify(schedule)
   })
 }
